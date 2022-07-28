76a9ee4ce1ab6f65d4b5cc7e36bb4b09cf598115
tadahirokin <kin@aees.kyushu-u.ac.jp>
2022-07-28 12:55:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Randomness_Education00/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Randomness_Education00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(412, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(393, 359);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "\r\n************************************************\r\n     Randomness Education Too" +
+    "l\r\n                              T. Kin, June 2018\r\n****************************" +
+    "********************\r\n\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 86);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "サイコロを\r\n100個振る";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 378);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 371);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(752, 345);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "分布測定";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(100, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 44);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "アヤしいサイを\r\n100個1000回振る";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(8, 98);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(180, 191);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "-------------\r\n";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(195, 7);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series4.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            series4.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(551, 332);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "100個を\r\n1000回振る";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.chart2);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(752, 345);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "減衰測定";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Lime;
+            this.textBox3.Location = new System.Drawing.Point(7, 118);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 220);
+            this.textBox3.TabIndex = 6;
+            // 
+            // chart2
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart2.Legends.Add(legend3);
+            this.chart2.Location = new System.Drawing.Point(194, 6);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(551, 332);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "chart2";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(6, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 59);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "サイコロを\r\n100個振る";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(7, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 41);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "リセット";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Randomness Education Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Randomness_Education00/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Randomness_Education00
+{
+
+
+    public partial class Form1 : Form
+    {
+        public BitmapData [] dice= new BitmapData [7];
+        public Bitmap[] bmp_dice = new Bitmap[7];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bmp_dice[0] = Properties.Resources._0;
+            //dice[0] = bmp_dice[0].LockBits(new Rectangle(0, 0, bmp_dice[0].Width, bmp_dice[0].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            bmp_dice[1] = Properties.Resources._1;
+            //dice[1] = bmp_dice[1].LockBits(new Rectangle(0, 0, bmp_dice[1].Width, bmp_dice[1].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            bmp_dice[2] = Properties.Resources._2;
+            //dice[2] = bmp_dice[2].LockBits(new Rectangle(0, 0, bmp_dice[2].Width, bmp_dice[2].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            bmp_dice[3] = Properties.Resources._3;
+            //dice[3] = bmp_dice[3].LockBits(new Rectangle(0, 0, bmp_dice[3].Width, bmp_dice[3].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            bmp_dice[4] = Properties.Resources._4;
+            //dice[4] = bmp_dice[4].LockBits(new Rectangle(0, 0, bmp_dice[4].Width, bmp_dice[4].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            bmp_dice[5] = Properties.Resources._5;
+            //dice[5] = bmp_dice[5].LockBits(new Rectangle(0, 0, bmp_dice[5].Width, bmp_dice[5].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            bmp_dice[6] = Properties.Resources._6;
+            //dice[6] = bmp_dice[6].LockBits(new Rectangle(0, 0, bmp_dice[6].Width, bmp_dice[6].Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            chart1.Series[0].IsVisibleInLegend = false;
+
+        }
+
+        public int[] hist = new int[100];
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //サイコロをふる
+            int[,] number = new int[10, 10];
+            int[] count = new int[7];
+
+            Random rnd = new System.Random();
+            for (int dice_y = 0; dice_y < 10; dice_y++)
+            {
+                for (int dice_x = 0; dice_x < 10; dice_x++)
+                {
+                    int intResult = rnd.Next(1, 7);
+                    number[dice_x, dice_y] = intResult;
+                    count[intResult]++;
+                }
+            }
+
+            hist[count[1]]++;
+
+
+            for(int i =0;i<100;i++)
+            {
+                chart1.Series[0].Points.AddXY(i, hist[i]);
+            }
+
+            Graphics g = pictureBox1.CreateGraphics();
+            for(int i=0; i<10;i++)
+            {
+                for(int j=0; j<10;j++)
+                {
+                    g.DrawImage(bmp_dice[number[j, i]], 30 * (j+1), 30 * (i+1), 20,20);
+                }
+            }
+
+            for(int i=1; i<7; i++)
+            {
+                textBox2.AppendText(i.ToString() + ": " + count[i].ToString() + "個 \n");
+            }
+            textBox2.AppendText(" ------------- \n");
+
+            //pictureBox1.Refresh();
+
+            /*bmp_dice[1].UnlockBits(dice[1]);
+            pictureBox1.Image = bmp_dice[1];
+            pictureBox1.Refresh();
+            //サイコロをふる
+            int [,] number= new int[10,10];
+            Random rnd = new System.Random();
+            for (int dice_y = 0; dice_y < 10; dice_y++)
+            {
+                for (int dice_x = 0; dice_x < 10; dice_x++)
+                {
+                    int intResult = rnd.Next(1,7);
+                    number[dice_x, dice_y] = intResult;
+                }
+            }
+            Bitmap bmp_dice_window = new Bitmap(360, 360);
+            BitmapData dice_window = bmp_dice_window.LockBits(new Rectangle(0, 0, bmp_dice_window.Width, bmp_dice_window.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            for(int dice_y=0; dice_y < 10; dice_y++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    for (int dice_x = 0; dice_x < 10; dice_x++)
+                    {
+                        for (int j = 0; j < 30; j++)
+                        {
+                            Int32 value = Marshal.ReadInt32(dice[number[dice_x, dice_y]].Scan0, j * 4 + 30 * 4 * i);
+                            byte r = (byte)(value & 0xff);
+                            byte g = (byte)((value >> 8) & 0xff);
+                            byte b = (byte)((value >> 16) & 0xff);
+                            value = r | g | b;
+                            Marshal.WriteInt32(dice_window.Scan0, ((30 + dice_x * 30) + j) * 4 + 360 * 4 * ((30 + dice_y * 30) + i), value);
+                        }
+                    }
+                }
+            }
+
+            bmp_dice_window.UnlockBits(dice_window);
+            pictureBox1.Image = bmp_dice_window;
+            pictureBox1.Refresh();*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            Random rnd = new System.Random();
+            for (int ii = 0; ii < 1000; ii++)
+            {
+                //サイコロをふる
+                int[,] number = new int[10, 10];
+                int[] count = new int[7];
+                for (int dice_y = 0; dice_y < 10; dice_y++)
+                {
+                    for (int dice_x = 0; dice_x < 10; dice_x++)
+                    {
+                        int intResult = rnd.Next(1, 7);
+                        number[dice_x, dice_y] = intResult;
+                        count[intResult]++;
+                    }
+                }
+
+                hist[count[1]]++;
+
+
+                for (int i = 0; i < 50; i++)
+                {
+                    chart1.Series[0].Points.AddXY(i, hist[i]);
+                }
+
+                if (ii % 10 == 0)
+                {
+                    Graphics g = pictureBox1.CreateGraphics();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            g.DrawImage(bmp_dice[number[j, i]], 30 * (j + 1), 30 * (i + 1), 20, 20);
+                        }
+                    }
+                }
+
+                /*
+                for (int i = 1; i < 7; i++)
+                {
+                    textBox2.AppendText(i.ToString() + ": " + count[i].ToString() + "個 \n");
+                }
+                textBox2.AppendText(" ------------- \n");*/
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new System.Random();
+            for (int ii = 0; ii < 1000; ii++)
+            {
+                //サイコロをふる
+                int[,] number = new int[10, 10];
+                int[] count = new int[7];
+                for (int dice_y = 0; dice_y < 10; dice_y++)
+                {
+                    for (int dice_x = 0; dice_x < 10; dice_x++)
+                    {
+                        int intResult = rnd.Next(1, 7);
+                        if (intResult == 1) { intResult = rnd.Next(1, 7); } //1の出る確率が36分の1
+                        number[dice_x, dice_y] = intResult;
+                        count[intResult]++;
+                    }
+                }
+
+                hist[count[1]]++;
+
+
+                for (int i = 0; i < 25; i++)
+                {
+                    chart1.Series[0].Points.AddXY(i, hist[i]);
+                }
+
+                if (ii % 10 == 0)
+                {
+                    Graphics g = pictureBox1.CreateGraphics();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            g.DrawImage(bmp_dice[number[j, i]], 30 * (j + 1), 30 * (i + 1), 20, 20);
+                        }
+                    }
+                }
+            }
+        }
+
+        public int dice_counter = 100;
+        public int trial_counter = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dice_counter < 2)
+            {
+                textBox3.AppendText("もう振れないよ！ \n");
+                return;
+            }
+
+            trial_counter++;
+
+            //サイコロをふる
+            int[,] number = new int[10, 10];
+            int[] count = new int[7];
+
+            Random rnd = new System.Random();
+
+            int temp = 0;
+            for (int dice_y = 0; dice_y < 10; dice_y++)
+            {
+                for (int dice_x = 0; dice_x < 10; dice_x++)
+                {
+                    if (temp < dice_counter)
+                    {
+                        int intResult = rnd.Next(1, 7);
+                        number[dice_x, dice_y] = intResult;
+                        count[intResult]++;
+                    }
+                    temp++;
+                }
+            }
+            temp = 0;
+
+
+
+            Graphics g = pictureBox1.CreateGraphics();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (temp < dice_counter)
+                    {
+                        g.DrawImage(bmp_dice[number[j, i]], 30 * (j + 1), 30 * (i + 1), 20, 20);
+                    }
+                    else
+                    {
+                        g.DrawImage(bmp_dice[0], 30 * (j + 1), 30 * (i + 1), 20, 20);
+                    }
+                    temp++;
+                }
+            }
+
+
+            textBox3.AppendText(trial_counter.ToString()+": "+ dice_counter.ToString() + "個中, 1は" + count[1].ToString() + "個 \n");
+            textBox3.AppendText(" ------------- \n");
+
+            chart2.Series[0].Points.AddXY(trial_counter, dice_counter);
+
+            dice_counter -= count[1];
+            button4.Text = "さいころを" + ""+
+                dice_counter.ToString() + "個振る";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dice_counter = 100;
+            trial_counter = 0;
+            chart2.Series[0].Points.Clear();
+        }
+    }
+}
